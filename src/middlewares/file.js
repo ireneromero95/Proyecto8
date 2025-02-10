fa1c47c517d301801851c2ff5c7a1315da029ff5
IreneRomero <irene_romero_95@hotmail.com>
2025-02-10 20:08:14 +0100

    Corregido una sola funcion

--- src/middlewares/file.js
+++ src/middlewares/file.js
@@ -2,27 +2,62 @@ const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-const storageSong = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'Songs',
-    allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'webp']
-  }
-});
+const createStorage = (folder, allowedFormats) => {
+  return new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: folder,
+      allowedFormats: allowedFormats
+    }
+  });
+};
 
-const storagePlatform = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'Platform',
-    allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'webp']
-  }
-});
+const storageSong = createStorage('Songs', [
+  'jpg',
+  'png',
+  'jpeg',
+  'gif',
+  'webp'
+]);
+const storagePlatform = createStorage('Platform', [
+  'jpg',
+  'png',
+  'jpeg',
+  'gif',
+  'webp'
+]);
 
 const uploadSong = multer({ storage: storageSong });
 const uploadPlatform = multer({ storage: storagePlatform });
 
 exports.uploadSong = uploadSong;
 exports.uploadPlatform = uploadPlatform;
+
+// const multer = require('multer');
+// const cloudinary = require('cloudinary').v2;
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+// const storageSong = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'Songs',
+//     allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'webp']
+//   }
+// });
+
+// const storagePlatform = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'Platform',
+//     allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'webp']
+//   }
+// });
+
+// const uploadSong = multer({ storage: storageSong });
+// const uploadPlatform = multer({ storage: storagePlatform });
+
+// exports.uploadSong = uploadSong;
+// exports.uploadPlatform = uploadPlatform;
 
 //Si aquí le cambiara el nombre a storage tendría que poner storage: elnombre, pero
 //funciona con short cut
